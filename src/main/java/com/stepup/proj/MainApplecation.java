ec0ac1ce4bc80691454eaa3decece018b3c0fe7c
ksher89 <ksher89@mail.ru>
2025-04-17 13:27:08 +0300

    ес

--- src/main/java/com/stepup/proj/MainApplecation.java
+++ src/main/java/com/stepup/proj/MainApplecation.java
@@ -3,5 +3,6 @@ package com.stepup.proj;
 public class MainApplecation {
     public static void main(String[] args) {
         System.out.println("Hello Word!");
+        System.out.println("1111");
     }
 }
