45237bbfdf17ddcf0476cc14569b47622263f8cd
ksher89 <ksher89@mail.ru>
2025-04-17 14:14:35 +0300

    добавлен ще коммит

--- src/main/java/com/stepup/proj/MainApplecation.java
+++ src/main/java/com/stepup/proj/MainApplecation.java
@@ -5,6 +5,7 @@ public class MainApplecation {
         System.out.println("Hello Word!");
         System.out.println("-----");
         System.out.println("110011");
+        System.out.println("111");
 
         System.out.println("222");
         System.out.println("00");
