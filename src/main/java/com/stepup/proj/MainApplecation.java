a97a98dbc2bd656b8918da9da528a55780c5c6a3
ksher89 <ksher89@mail.ru>
2025-04-17 13:57:18 +0300

    сделали замену

--- src/main/java/com/stepup/proj/MainApplecation.java
+++ src/main/java/com/stepup/proj/MainApplecation.java
@@ -3,10 +3,12 @@ package com.stepup.proj;
 public class MainApplecation {
     public static void main(String[] args) {
         System.out.println("Hello Word!");
+        System.out.println("-----");
         System.out.println("110011");
 
         System.out.println("222");
-        System.out.println("333");
+        System.out.println("00");
+
 
     }
 }
